2096901bfcc3174cd8e301f6db77af1a7748bc5f
Lorenzolivacode <lorenzo.oliva.1308@gmail.com>
2024-10-08 19:08:47 +0200

    fix: delete css select

--- app/[locale]/(components)/(atoms)/SelectLanguage-client/SelectLanguage.tsx
+++ app/[locale]/(components)/(atoms)/SelectLanguage-client/SelectLanguage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { ChangeEvent } from "react";
-import "./SelectLanguage.css";
 
 import { useLocale, useTranslations } from "next-intl";
 import { useRouter } from "next/navigation";
