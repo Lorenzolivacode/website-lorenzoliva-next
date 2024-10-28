506ad167fbcb3184184279853cfd4cf62db1251b
Lorenzolivacode <lorenzo.oliva.1308@gmail.com>
2024-10-28 11:57:08 +0100

    fix

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
-"use client";
-
 import { redirect } from "next/navigation";
-import { useLocale } from "next-intl";
 
 export default function RootPage() {
-  const locale = useLocale();
   redirect("/it");
 }
